2a6f08b348721f2ab937d6097c1f4dde773a9b5c
sivachakradhark <sivachakradhark2002@gmail.com>
2024-07-11 19:15:54 +0530

    Delete two.js

--- two.js
+++
@@ -1,8 +0,0 @@
-// brand name
-var brand_name="killer";
-var product_name="running shoes";
-var avail='Y'
-console.log(typeof brand_name);
-console.log(typeof product_name);
-console.log(typeof avail);
- 
